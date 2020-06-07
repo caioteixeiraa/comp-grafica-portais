e5c14b578586a63f6e9b5b03bf79bdb407735bf1
lucas paiva <lucas.gomes.de.paiva@usp.br>
2020-06-07 15:46:40 -0300

    Attempt at adding teleporting logic. Gone wrong. Still need to find out how to get the collidingBody position from the protal script

--- Portal3/PortalA.gd
+++ Portal3/PortalA.gd
@@ -1,16 +1,31 @@
 extends Spatial
 
 
+const MAX_CONTACTS_REPORTED = 4
+
+var staticBody: RigidBody
+var collidingBodies
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready(): 
+	get_node("RigidBody").set_max_contacts_reported(MAX_CONTACTS_REPORTED)
+	staticBody = get_node("RigidBody")
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	var collidingBodies = staticBody.get_colliding_bodies()
+	for collidingBody in collidingBodies:
+		if collidingBody is KinematicBody:	
+			shouldTeleport(collidingBody)
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func shouldTeleport(collidingBody: KinematicBody):
+	# TODO get collidingBody position (should be a Vector3)
+	# TODO calculate if it is inside the CollisionShape
+	# TODO get collidingBody position relative to PortalA position (relativePos = collidingBody.position - parentNode.position)
+	# TODO if it is, set collidingBody position to (PortalB.position + relativePos)
+	pass
--- Portal3/PortalPair.gd
+++ Portal3/PortalPair.gd
@@ -1,5 +1,7 @@
 extends Node
 
+onready var portals = [$PortalA, $PortalB]
+
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,6 +12,16 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
+
+# Move body from portal from_portal to portal to_por
+func teleport_body(body: PhysicsBody, from_portal: Spatial, to_portal: Spatial):
+	var body_pos = body.global_transform
+	var body_speed = body.linear_velocity
+	body_pos = body_pos - from_portal.global_transform + to_portal.global_transform
+	body.global_transform = body_pos
+	return
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
